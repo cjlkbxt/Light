68960c04a4296c88481676e3f894d9b06e530d9e
chenjialei <cjlkbxt@163.com>
2021-06-29 09:10:29 +0800

    定位优化

--- app/src/main/java/com/kobe/light/ui/scan/ScanActivity.java
+++ app/src/main/java/com/kobe/light/ui/scan/ScanActivity.java
@@ -419,10 +419,8 @@ public class ScanActivity extends BaseActivity<ScanContract.presenter> implement
                         mGpsLatitude = location.getLatitude() + "";
                         mGpsLongitude = location.getLongitude() + "";
                     }*/
-                    initLocationOption();
+                    startLocation();
 //                    getDistance()
-                    tv_to_position.setTextColor(getResources().getColor(R.color.color_grey));
-                    mTvSubmit.setEnabled(true);
                 }
 
                 break;
@@ -1014,7 +1012,7 @@ public class ScanActivity extends BaseActivity<ScanContract.presenter> implement
      */
     public LocationClient mLocationClient = null;
 
-    private void initLocationOption() {
+    private void startLocation() {
         //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
         mLocationClient = new LocationClient(getApplicationContext());
         //声明LocationClient类实例并配置定位参数
@@ -1022,36 +1020,25 @@ public class ScanActivity extends BaseActivity<ScanContract.presenter> implement
         MyLocationListener myLocationListener = new MyLocationListener();
         //注册监听函数
         mLocationClient.registerLocationListener(myLocationListener);
-        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-        //可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-        locationOption.setCoorType("gcj02");
+        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        locationOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
         //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
         //        locationOption.setScanSpan(1000);
-        //可选，设置是否需要地址信息，默认不需要
-        locationOption.setIsNeedAddress(true);
-        //可选，设置是否需要地址描述
-        locationOption.setIsNeedLocationDescribe(true);
-        //可选，设置是否需要设备方向结果
-        locationOption.setNeedDeviceDirect(false);
+        locationOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        locationOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
+        locationOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
         //可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
 //        locationOption.setLocationNotify(true);
-        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-        locationOption.setIgnoreKillProcess(true);
-        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-        locationOption.setIsNeedLocationDescribe(true);
-        //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        locationOption.setIsNeedLocationPoiList(true);
-        //可选，默认false，设置是否收集CRASH信息，默认收集
-        locationOption.SetIgnoreCacheException(false);
-        //可选，默认false，设置是否开启Gps定位
-        locationOption.setOpenGps(true);
-        //可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
-        locationOption.setIsNeedAltitude(false);
+        locationOption.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        locationOption.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        locationOption.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        locationOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        locationOption.setOpenGps(true);  //可选，默认false，设置是否开启Gps定位
+        locationOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
         //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
-        locationOption.setOpenAutoNotifyMode();
+//        locationOption.setOpenAutoNotifyMode();
         //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
-        locationOption.setOpenAutoNotifyMode(3000, 1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
+//        locationOption.setOpenAutoNotifyMode(3000, 1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
         //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
         mLocationClient.setLocOption(locationOption);
         //开始定位
@@ -1067,7 +1054,6 @@ public class ScanActivity extends BaseActivity<ScanContract.presenter> implement
             //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
             //以下只列举部分获取经纬度相关（常用）的结果信息
             //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
-
             //获取纬度信息
             double latitude = location.getLatitude();
             //获取经度信息
@@ -1077,18 +1063,13 @@ public class ScanActivity extends BaseActivity<ScanContract.presenter> implement
 //            Log.i("cjl", "onReceiveLocation1: " + latitude);
 //            Log.i("cjl", "onReceiveLocation2: " + longitude);
             if (!TextUtils.equals(mGpsLatitude, "4.9E-324") && !TextUtils.equals(mGpsLongitude, "4.9E-324")) {
-                ToastUtil.showShort(ScanActivity.this, "定位成功!");
+                ToastUtil.showShort(ScanActivity.this, "获取定位成功!");
+                tv_to_position.setTextColor(getResources().getColor(R.color.color_grey));
+                mTvSubmit.setEnabled(true);
             } else {
-                ToastUtil.showShort(ScanActivity.this, "定位失败，请稍后重试!");
+                ToastUtil.showShort(ScanActivity.this, "获取定位失败，请稍后重试!");
             }
             mLocationClient.stop();
-//            //获取定位精度，默认值为0.0f
-//            float radius = location.getRadius();
-//            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
-//            String coorType = location.getCoorType();
-//            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-//            int errorCode = location.getLocType();
-
         }
     }
 }
